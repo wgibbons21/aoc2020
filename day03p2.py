f8a84ece8f21e48489a2fccdf14f8bdd7478285b
wgibbons21 <wgibbons@gmail.com>
2020-12-05 23:09:22 -0500

    Day 3 part 2 solved

--- day03p2.py
+++ day03p2.py
@@ -20,27 +20,30 @@ def toboggan_tree_count(input_map, xmove, ymove):
     # loop through each row as determined by the ymove
     # print("Len of input map", len(input_map))
     # i starts at 1 since we don't evaluate row 0
-    for i in range(1, len(input_map)):
-        print(i, x)
+    if input_map[0][0] == '#':
+        trees_encountered += 1
+    #start with i on the first row move (ymove)
+    for i in range(ymove, len(input_map), ymove):
+        print(i)
         # if the x move takes us out of bounds, wrap around the array
-        if xmove + x >= len(input_map[0]):
+        if xmove + x > len(input_map[0])-1:
             x = xmove + x - len(input_map[0])
         # no wrapping needed
         else:
-            print("Evaluating else")
             x = xmove + x
         # did we run into a tree (#)
+        print("Checking:", i, x)
         if input_map[i][x] == '#':
-            print("Encountered a tree at", i, x)
             trees_encountered += 1
+    #print(input_map[i])
     return trees_encountered
 
 # what is the slope we are using to traverse the map
 #x_move = 3
 #y_move = 1
 
-print(toboggan_tree_count(contents, 1, 1))
-print(toboggan_tree_count(contents, 3, 1))
-print(toboggan_tree_count(contents, 5, 1))
-print(toboggan_tree_count(contents, 7, 1))
-print(toboggan_tree_count(contents, 1, 2))
+print("Answer:", toboggan_tree_count(contents, 1, 1) *
+      toboggan_tree_count(contents, 3, 1) *
+      toboggan_tree_count(contents, 5, 1) *
+      toboggan_tree_count(contents, 7, 1) *
+      toboggan_tree_count(contents, 1, 2))
